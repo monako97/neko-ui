138acf2f5d98ecf6304a803bd156dc28e7d7ff79
moneko <poi.nyaa@qq.com>
2022-10-13 16:27:55 +0800

    fix: commonJS css

--- components/utils/classname.ts
+++ components/utils/classname.ts
@@ -1,6 +1,6 @@
 import variables from '../variables.less';
 
-export const getPrefixCls = (className: string, prefixCls = variables.prefixCls) =>
+export const getPrefixCls = (className: string, prefixCls = variables.prefixCls || 'neko') =>
   `${prefixCls}-${className}`;
 
 export const classNames = (cls?: (string | undefined | null | boolean | number)[]) =>
