c4db4591249351176c2830f9dba50e379460b0e6
moneko <poi.nyaa@qq.com>
2022-10-19 15:24:47 +0800

    fix: prefixCls

--- components/get-prefix-cls/index.ts
+++ components/get-prefix-cls/index.ts
@@ -1,6 +1,7 @@
 import css from './index.less';
 import { getPrefixCls as prefixCls } from '@moneko/common';
 
-const getPrefixCls = (className: string, prefix = css.prefixCls) => prefixCls(className, prefix);
+const getPrefixCls = (className: string, prefix = css?.prefixCls || 'neko') =>
+  prefixCls(className, prefix);
 
 export default getPrefixCls;
