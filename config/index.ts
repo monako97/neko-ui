1459ea22544966650d11c248cc738bf406d76038
moneko <poi.nyaa@qq.com>
2023-03-17 12:20:14 +0800

    docs: 文档中的hash路由替换

--- config/index.ts
+++ config/index.ts
@@ -7,6 +7,22 @@ const conf: PartialConfigType = {
   publicPath: `/${PACKAGENAME}`,
   htmlPluginOption: {
     favicon: './site/assets/images/favicon.ico',
+    tags: [
+      {
+        textContent: `
+          (function(l) {
+            if (l.search[1] === '/' ) {
+              var decoded = l.search.slice(1).split('&').map(function(s) { 
+                return s.replace(/~and~/g, '&')
+              }).join('?');
+              window.history.replaceState(null, null,
+                  l.pathname.slice(0, -1) + decoded + l.hash
+              );
+            }
+          }(window.location))
+        `,
+      },
+    ],
   },
   designSize: 1920,
   fallbackCompPath: '@/components/fallback',
