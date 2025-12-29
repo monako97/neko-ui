1cd6418acc2ab27b0e5ed5784c66c1d4dca4e5f2
Moneko <meow.moneko@gmail.com>
2025-12-29 18:00:25 +0800

    feat: 添加PWA截图

--- config/index.ts
+++ config/index.ts
@@ -17,6 +17,52 @@ const conf: Partial<ConfigType> = {
   devServer: {
     https: true,
   },
+  manifest: {
+    screenshots: [
+      {
+        src: 'https://h5static.dewucdn.com/node-common/672cb705-6a2b-7cde-2764-91e7af356478.webp',
+        sizes: '390x844',
+        type: 'image/webp',
+        form_factor: 'narrow',
+        label: 'Home',
+      },
+      {
+        src: 'https://h5static.dewucdn.com/node-common/f1cd0113-adb7-de1a-ff81-2e386b3a291c.webp',
+        sizes: '390x844',
+        type: 'image/webp',
+        form_factor: 'narrow',
+        label: '图片查看器',
+      },
+      {
+        src: 'https://h5static.dewucdn.com/node-common/578d1260-320e-3939-6457-451a48f80d84.webp',
+        sizes: '1280x720',
+        type: 'image/webp',
+        form_factor: 'wide',
+        label: '快速开始',
+      },
+      {
+        src: 'https://h5static.dewucdn.com/node-common/037a692b-8e40-58c5-3ae5-3bd2afb376c7.webp',
+        sizes: '1280x720',
+        type: 'image/webp',
+        form_factor: 'wide',
+        label: 'Markdown',
+      },
+      {
+        src: 'https://h5static.dewucdn.com/node-common/12d19810-0339-43b3-b492-2896a6e0bd06.webp',
+        sizes: '1280x720',
+        type: 'image/webp',
+        form_factor: 'wide',
+        label: '数学公式',
+      },
+      {
+        src: 'https://h5static.dewucdn.com/node-common/569435a6-05ba-9578-41e3-bed35097eb47.webp',
+        sizes: '1280x720',
+        type: 'image/webp',
+        form_factor: 'wide',
+        label: '二维码',
+      },
+    ],
+  },
 };
 
 export default conf;
